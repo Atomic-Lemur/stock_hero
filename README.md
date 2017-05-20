fcb347c1e498a8fd1a0477cc716ec574bc9138eb
Jon Link <jonlink@gmail.com>
2017-05-20 17:23:36 -0400

    added materialize framework

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 #Stock Hero
+
+### ABOUT
+--------------------
 This is (or rather will be) a super basic stock fantasy game. This particular iteration does not utilize any database, but is instead a dead simple session based game.
+
+### FEATURES
+--------------------
+*random stocks with prices that move up and down
--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if($action=='reset'){
 }
 
 if(!isset($_SESSION['stocks'])){
-  $stock_count = $_GET['stock_count'];
+  $stock_count = isset($_GET['stock_count']) ? $_GET['stock_count'] : rand(5, 10);
   for($x=0; $x<$stock_count; ++$x){
     $_SESSION['stocks'][] = new Stock();
   }
@@ -31,18 +31,64 @@ if(!isset($_SESSION['stocks'])){
 
 <!DOCTYPE html>
 <html>
-<head>
+<html lang="en">
+  <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Stock Hero</title>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+  <style>
+    :root {
+      --nav-background-color: #93caf2;
+      --nav-font-color: #fff;
+      --body-background-color: #fff;
+      --body-font-color: #333;
+    }
+    body{
+      background: var(--body-background-color);
+      color: var(--body-font-color);
+    }
+    .nav_color{
+      background: var(--nav-background-color);
+      color: var(--nav-font-color);
+    }
+    .nav-wrapper a{
+      color: var(--nav-font-color);
+    }
+    .stock{
+      line-height: 2rem;
+      font-size: 1.2rem;
+    }
+    .dead_stock{
+      color: #882b12;
+      text-decoration: line-through;
+    }
+  </style>
 </head>
 <body>
 
-  <h2>The Market</h2>
-  <ul id="market_list">
-  <?
-  foreach($_SESSION['stocks'] as $one_stock){
-    echo '<li>'.$one_stock->name.' ('.$one_stock->symbol.') $'.$one_stock->price.'</li>';
-  }
-  ?>
-  </ul>
+  <nav>
+    <div class="nav-wrapper nav_color">
+      <a href="#!" class="brand-logo center"><i class="material-icons">trending_up</i>Stock Hero</a>
+    </div>
+  </nav>
+
+  <div class="container">
+    <h2>The Market</h2>
+    <ul id="market_list">
+      <?
+      foreach($_SESSION['stocks'] as $one_stock){
+        echo '<li class="stock'.($one_stock->active ? null : ' dead_stock').'">'.$one_stock->name.' ('.$one_stock->symbol.') $'.$one_stock->price.'</li>';
+      }
+      ?>
+    </ul>
+  </div><!--/.container -->
+
 </body>
 </html>
