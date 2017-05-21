0a6acbda98e871395c594fdfc98f4fb1c3b07c55
Jon Link <jonlink@gmail.com>
2017-05-20 20:43:51 -0400

    added a way to sell stock from portfolio

--- class.Portfolio.php
+++ class.Portfolio.php
@@ -9,9 +9,18 @@ class Portfolio{
     $this->bank = $this->bank + $amount;
   }
 
-  function add_stock($symbol, $qty){
+  function update_stock($symbol, $qty){
     if(!$symbol || !$qty) return false;
-    $this->stocks[$symbol] = $qty + $this->stocks[$symbol];
+
+    $new_amount = $this->stocks[$symbol] + $qty;
+    if($new_amount == 0){
+      unset($this->stocks[$symbol]);
+    }elseif($new_amount < 0){
+      return false;
+    }else{
+      $this->stocks[$symbol] = $new_amount;
+    }
+    return true;
   }
 
   function buy_stock($symbol, $qty, $price){
@@ -19,13 +28,30 @@ class Portfolio{
 
     $cost = $qty * $price * -1;
     if(($this->bank + $cost) > 0){
-      $this->add_stock($symbol, $qty);
+      $this->update_stock($symbol, $qty);
       $this->update_bank($cost);
       return array("result"=> true, "message" => "You just bought ".$qty." shares of ".$symbol.".");
     }else{
       return array("result"=> false, "message" => "You don't have enough money.");
     }
   }
+
+  function sell_stock($symbol, $qty, $price){
+    $missing = null;
+    if(!$symbol) $missing .= 'stock information, ';
+    if(!$qty) $missing .= 'number of shares to sell, ';
+    if(!$price) $missing .= 'stock price, ';
+    if($missing) return array("result"=> false, "message" => "Info missing, unable to make the sale. We need ".trim($missing, ', ').".");
+
+    $income = $qty * $price;
+    if( $this->update_stock($symbol, ($qty * -1)) ){
+      $this->update_bank($income);
+      return array("result"=> true, "message" => "You just sold ".$qty." shares of ".$symbol." for $".$income.".");
+    }else{
+      return array("result"=> false, "message" => "Unable to complete the sale.");
+    }
+  }
+
 }
 
 ?>
--- index.php
+++ index.php
@@ -15,11 +15,19 @@ if(isset($_POST['action'])){
 if($action=='restart'){
   session_destroy();
   session_start();
+
 }elseif($action=='buy'){
   $message = $_SESSION['my_portfolio']->buy_stock(
     $_POST['buy_stock_symbol'],
     $_POST['buy_stock_amount'],
     $_SESSION['stocks'][$_POST['buy_stock_symbol']]->price
+  );
+
+}elseif($action=='sell'){
+  $message = $_SESSION['my_portfolio']->sell_stock(
+    $_GET['sell_stock_symbol'],
+    $_SESSION['my_portfolio']->stocks[$_GET['sell_stock_symbol']],
+    $_SESSION['stocks'][$_GET['sell_stock_symbol']]->price
   );
 }
 
@@ -32,7 +40,7 @@ if(!isset($_SESSION['stocks'])){
     $_SESSION['stocks'][$stock->symbol] = $stock;
   }
 
-}else{
+}elseif(!$action){
   foreach($_SESSION['stocks'] as $one_stock){
     $one_stock->update_price(rand(0, 1));
   }
@@ -86,6 +94,11 @@ if(!isset($_SESSION['stocks'])){
       color: #882b12;
       text-decoration: line-through;
     }
+    #buy_stock_form{
+      margin-top: 5rem;
+      border:1px solid #888;
+      border-radius: 5px;
+    }
   </style>
 </head>
 <body>
@@ -98,7 +111,7 @@ if(!isset($_SESSION['stocks'])){
     <div class="nav-wrapper nav_color">
       <a href="#!" class="brand-logo center"><i class="material-icons">trending_up</i>Stock Hero</a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="?" title="continue without buying or selling"><i class="material-icons">skip_next</i></a></li>
+        <li><a href="?" title="continue"><i class="material-icons">play_arrow</i></a></li>
         <li><a href="?action=restart" title="restart"><i class="material-icons">refresh</i></a></li>
       </ul>
     </div>
@@ -111,12 +124,15 @@ if(!isset($_SESSION['stocks'])){
         <h3>Your Portfolio</h3>
         <p>Bank: $<?php echo $_SESSION['my_portfolio']->bank ?></p>
 
-        <table id="portfolio">
-          <tr>
-            <th class="center">Symbol</th>
-            <th class="center">Qty</th>
-            <th class="center">Value</th>
-          </tr>
+        <table id="portfolio" class="striped">
+          <thead>
+            <tr>
+              <th class="center">Symbol</th>
+              <th class="center">Qty</th>
+              <th class="center">Value</th>
+              <th class="center">Sell Stock</th>
+            </tr>
+          </thead>
           <?
           foreach($_SESSION['my_portfolio']->stocks as $stock_symbol => $stock_qty){
             echo '
@@ -124,43 +140,53 @@ if(!isset($_SESSION['stocks'])){
               <td class="center stock_symbol">'.$stock_symbol.'</td>
               <td class="center">'.$stock_qty.'</td>
               <td class="center">$'.number_format(($_SESSION['stocks'][$stock_symbol]->price * $stock_qty), 2).'</td>
+              <td class="center"><a href="?action=sell&sell_stock_symbol='.$stock_symbol.'" title="sell all of this stock"><i class="material-icons">shopping_basket</i></a></td>
             </tr>';
           }
           ?>
         </table>
 
-        <form action="" method="POST">
-          <div class="input-field col s4">
-            <select name="buy_stock_symbol">
-            <?php
-              foreach($_SESSION['stocks'] as $one_stock){
-                echo '<option value="'.$one_stock->symbol.'" title="'.$one_stock->name.' ('.$one_stock->symbol.')">'.$one_stock->symbol.'</option>';
-              }
-            ?>
-            </select>
-          </div>
+        <form action="" method="POST" id="buy_stock_form" class="col s12">
+          <div class="row">
+            <h5 class="col s12">buy stock</h5>
+          </div><!--row-->
 
-          <div class="input-field col s4">
-            <input name="buy_stock_amount" class="center" placeholder="amount" />
-          </div>
+          <div class="row center">
+            <div class="input-field col s5">
+              <select name="buy_stock_symbol">
+              <?php
+                foreach($_SESSION['stocks'] as $one_stock){
+                  echo '<option value="'.$one_stock->symbol.'" title="'.$one_stock->name.' ('.$one_stock->symbol.')">'.$one_stock->symbol.'</option>';
+                }
+              ?>
+              </select>
+            </div>
 
-          <div class="input-field col s4">
-            <button type="submit" class="btn-floating btn-medium waves-effect waves-light red"><i class="material-icons">add</i></button>
-          </div>
-          <input type="hidden" name="action" value="buy" />
+            <div class="input-field col s5">
+              <input name="buy_stock_amount" class="center" placeholder="amount" />
+            </div>
+
+            <div class="input-field col s2">
+              <button type="submit" class="btn-floating btn-medium waves-effect waves-light red"><i class="material-icons">add</i></button>
+            </div>
+            <input type="hidden" name="action" value="buy" />
+          </div><!--row-->
         </form>
 
       </div><!--col-->
 
       <div class="col s12 m6">
         <h3>The Market</h3>
-        <table id="market">
-          <tr>
-            <th>Company</th>
-            <th>Symbol</th>
-            <th>Price</th>
-            <th>Change</th>
-          </tr>
+        <table id="market" class="striped">
+          <thead>
+            <tr>
+              <th>Company</th>
+              <th>Symbol</th>
+              <th>Price</th>
+              <th>Change</th>
+            </tr>
+          </thead>
+
           <?
           foreach($_SESSION['stocks'] as $one_stock){
             echo '
